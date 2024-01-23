beda7f6ed97f75d28d6289468f4836ce54bc4c74
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-01-23 23:11:32 +0400

    Update dependencies and enhance SEO configuration
    Updated various dependencies to latest versions including `astro-icon`, and added new dependencies like `@astrojs/sitemap` and `astro-robots-txt`. Further enhanced the SEO configuration by generating a robots.txt file and sitemap, these changes allow for better visibility and indexation by search engines.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,38 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:4321',
   prefetch: true,
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), robotsTxt({
+    policy: [{
+      userAgent: 'Googlebot',
+      allow: '/',
+      disallow: '',
+      crawlDelay: 10
+    }, {
+      userAgent: 'Yandex',
+      allow: '/',
+      disallow: '',
+      crawlDelay: 2
+    }, {
+      userAgent: 'archive.org_bot',
+      allow: '/',
+      disallow: '',
+      crawlDelay: 2
+    }, {
+      userAgent: '*',
+      allow: '',
+      disallow: '/'
+    }]
+  }), sitemap(), icon()],
   output: 'server',
   experimental: {
-    clientPrerender: true,
-  },
+    clientPrerender: true
+  }
 });
