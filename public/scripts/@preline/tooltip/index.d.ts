149397c0cad2ce32139e630a64230a075c6c0254
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-04-28 02:45:57 +0400

    Add accordion, collapse, stepper, tooltip, tabs JavaScript modules

---
+++ public/scripts/@preline/tooltip/index.d.ts
@@ -0,0 +1,53 @@
+
+export interface ITooltip {
+	options?: {};
+	show(): void;
+	hide(): void;
+}
+export interface IBasePlugin<O, E> {
+	el: E;
+	options?: O;
+	events?: {};
+}
+declare class HSBasePlugin<O, E = HTMLElement> implements IBasePlugin<O, E> {
+	el: E;
+	options: O;
+	events?: any;
+	constructor(el: E, options: O, events?: any);
+	createCollection(collection: any[], element: any): void;
+	fireEvent(evt: string, payload?: any): any;
+	on(evt: string, cb: Function): void;
+}
+export interface ICollectionItem<T> {
+	id: string | number;
+	element: T;
+}
+declare class HSTooltip extends HSBasePlugin<{}> implements ITooltip {
+	private readonly toggle;
+	content: HTMLElement | null;
+	readonly eventMode: string;
+	private readonly preventPopper;
+	private popperInstance;
+	private readonly placement;
+	private readonly strategy;
+	constructor(el: HTMLElement, options?: {}, events?: {});
+	private init;
+	private enter;
+	private leave;
+	private click;
+	private focus;
+	private buildPopper;
+	show(): void;
+	hide(): void;
+	static getInstance(target: HTMLElement | string, isInstance?: boolean): HTMLElement | ICollectionItem<HSTooltip>;
+	static autoInit(): void;
+	static show(target: HTMLElement): void;
+	static hide(target: HTMLElement): void;
+	static on(evt: string, target: HTMLElement, cb: Function): void;
+}
+
+export {
+	HSTooltip as default,
+};
+
+export {};
