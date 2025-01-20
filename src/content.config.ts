bdd1e518044e0ee87d7f28503e33b6ec5848d9cd
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2025-01-20 13:49:51 +0400

    feat: update Astro to v5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,16 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from 'astro/loaders';
 
-const recipesCollection = defineCollection({
-    type: "content",
+const recipes = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/recipes" }),
     schema: z.object ({
     title: z.string(),
     description: z.string(),
     recipeNotes: z.array(z.string()).optional(),
     pubDate: z.date(),
     cookingTime: z.number(),
+    image: z.string(),
+    imageAlt: z.string(),
     author: z.string().default('anonymous'),
     tags: z.array(z.string()).optional(),
     steps: z.array(
@@ -28,6 +31,4 @@ const recipesCollection = defineCollection({
     }),
 });
 
-export const collections = {
-    'recipes': recipesCollection,
-};
+export const collections = { recipes };
